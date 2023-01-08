0d7ba48a8d56a90b76e57e0abec91e1ae0b09f99
Francisco Ramos <jorge.ramos.francisco.15@gmail.com>
2023-01-08 11:06:48 -0300

    ADD Message on firestore read

--- src/app/services/fire.service.ts
+++ src/app/services/fire.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import {
   Firestore, doc, setDoc, serverTimestamp, DocumentReference, DocumentData, WithFieldValue,
   collection, query, QueryConstraint, CollectionReference, onSnapshot, docSnapshots, Unsubscribe, orderBy,
@@ -8,6 +8,7 @@ import { ToastrService } from 'ngx-toastr';
 import { map, Observable } from 'rxjs';
 import { GralDoc } from '../models/docs.model';
 import { ManagementService } from './management.service';
+import sizeof from 'firestore-size';
 
 @Injectable({
   providedIn: 'root'
@@ -71,6 +72,10 @@ export class FireService {
   doc$(path: string): Observable<any> {
     return docSnapshots(this.doc(path)).pipe(
       map((doc) => {
+        if (isDevMode()) {
+          const bytes = sizeof({ ...doc.data() });
+          this.managementService.log(`Reading doc ${doc.id} - ${this.formatBytes(bytes)}`);
+        }
         return {
           id: doc.id,
           ...doc.data(),
@@ -83,7 +88,10 @@ export class FireService {
 
   onSnapshotDoc$(path: string, callback: Function): Unsubscribe {
     return onSnapshot(this.doc(path), (doc) => {
-      if (!doc.exists()) return callback(null);
+      if (isDevMode()) {
+        const bytes = sizeof({ ...doc.data() });
+        this.managementService.log(`Reading doc ${doc.id} - ${this.formatBytes(bytes)}`);
+      }
       const finalData: GralDoc = {
         id: doc.id,
         ...doc.data(),
@@ -102,11 +110,18 @@ export class FireService {
     return onSnapshot(queryFn ? this.col(path, queryFn) : this.col(path), (querySnapshot) => {
       if (querySnapshot.empty) return callback([]);
       const finalData: GralDoc[] = querySnapshot.docs.map((doc) => {
+
+        if (isDevMode()) {
+          const bytes = sizeof(doc.data());
+          this.managementService.log(`Reading doc ${doc.id} - ${this.formatBytes(bytes)}`);
+        }
+
         return {
           id: doc.id,
           ...doc.data(),
           path: doc.ref.path,
           exists: doc.exists(),
+          fromCache: querySnapshot.metadata.fromCache,
         }
       });
       return callback(finalData);
@@ -114,7 +129,19 @@ export class FireService {
       (error) => {
         this.toastr.error('Error getting new data', 'Error');
         this.managementService.error('Error onSnapshotCol$ fireService', error);
-      });
+      })
+  }
+
+  private formatBytes(bytes: number, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
 
 }
